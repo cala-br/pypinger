4a3cf6df5ac9e5f91431e1ec27be8598412a6577
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-05-09 15:32:41 +0200

    Refactor

--- pinger/pinger.py
+++ pinger/pinger.py
@@ -304,119 +304,119 @@ class Pinger:
 
 #region Main
 async def main():
+  """
+  Pings each host passed from 
+  command line
+  """
+  import colorama
+  from argparse import ArgumentParser
+  from colorama import Fore, Style
+  
+  logging.basicConfig(
+    level  = logging.INFO,
+    format = "%(message)s")
+
+  colorama.init(autoreset=True)
+
+  #region Argparse init
+  parser = ArgumentParser(
+    description = "Pings the given hosts.")
+
+  parser.add_argument('hosts',
+    help  = 'A list of hosts to ping.',
+    nargs = '+',
+    type  = str)
+
+  parser.add_argument('-t', 
+    default = 5,
+    help    = 'Sets the timeout for each ping (not for DNS resolution).',
+    dest    = 'timeout',
+    type    = float)
+
+  parser.add_argument('--async',
+    default = False,
+    action  = 'store_true',
+    help    = 'Execute each ping concurrently.',
+    dest    = 'pingAsync')
+
+  parser.add_argument('-n',
+    default = 1,
+    help    = 'The number of pings that are executed for each host.',
+    dest    = 'repeat',
+    type    = int)
+
+  parser.add_argument('-r',
+    default = False,
+    action  = 'store_true',
+    help    = 'Whether to resolve the hosts using the DNS or not.',
+    dest    = 'resolve')
+
+  parser.add_argument('-c',
+    default = False,
+    action  = 'store_true',
+    help    = 'Ping indefinetely.',
+    dest    = 'continuous')
+
+  parser.add_argument('--delay',
+    default = 1,
+    help    = 'Delay between continuous or repeated pings. Default is 1 second.',
+    dest    = 'delay',
+    type    = float)
+  #endregion
+
+  try:
+    args = parser.parse_args()
+  except:
+    return
+
+  #region Log ping result
+  def logPingResult(res : PingResult) -> None:
     """
-    Pings each host passed from 
-    command line
+    Logs a formatted representation of a ping
     """
-    import colorama
-    from argparse import ArgumentParser
-    from colorama import Fore, Style
-    
-    logging.basicConfig(
-        level  = logging.INFO,
-        format = "%(message)s")
+    status = \
+      (Fore.GREEN + f"{'online':7}") if res.status else (Fore.RED + "offline")
 
-    colorama.init(autoreset=True)
+    status += Style.RESET_ALL
 
-    #region Argparse init
-    parser = ArgumentParser(
-        description = "Pings the given hosts.")
+    msg = \
+      f"{f'({res.hostname:^16}, {res.remoteAddress:^16})':32} -> {status} [delay: {res.delay}]"
 
-    parser.add_argument('hosts',
-        help  = 'A list of hosts to ping.',
-        nargs = '+',
-        type  = str)
+    logging.info(msg)
+  #endregion
 
-    parser.add_argument('-t', 
-        default = 5,
-        help    = 'Sets the timeout for each ping (not for DNS resolution).',
-        dest    = 'timeout',
-        type    = float)
+  #region Exec pings
+  async def execPings(sleep : bool = True):
+    # Async ping
+    if args.pingAsync:
+      results = await Pinger.pingAllAsync(
+        args.hosts,
+        args.timeout,
+        args.resolve)
 
-    parser.add_argument('--async',
-        default = False,
-        action  = 'store_true',
-        help    = 'Execute each ping concurrently.',
-        dest    = 'pingAsync')
+    # Synchronous ping
+    else:
+      results = Pinger.pingAll(
+        args.hosts,
+        args.timeout,
+        args.resolve)
 
-    parser.add_argument('-n',
-        default = 1,
-        help    = 'The number of pings that are executed for each host.',
-        dest    = 'repeat',
-        type    = int)
+    for res in results:
+      logPingResult(res)
 
-    parser.add_argument('-r',
-        default = False,
-        action  = 'store_true',
-        help    = 'Whether to resolve the hosts using the DNS or not.',
-        dest    = 'resolve')
-
-    parser.add_argument('-c',
-        default = False,
-        action  = 'store_true',
-        help    = 'Ping indefinetely.',
-        dest    = 'continuous')
-
-    parser.add_argument('--delay',
-        default = 1,
-        help    = 'Delay between continuous or repeated pings. Default is 1 second.',
-        dest    = 'delay',
-        type    = float)
-    #endregion
-
-    try:
-        args = parser.parse_args()
-    except:
-        return
-
-    #region Log ping result
-    def logPingResult(res : PingResult) -> None:
-        """
-        Logs a formatted representation of a ping
-        """
-        status = \
-            (Fore.GREEN + f"{'online':7}") if res.status else (Fore.RED + "offline")
-
-        status += Style.RESET_ALL
-
-        msg = \
-            f"{f'({res.hostname:^16}, {res.remoteAddress:^16})':32} -> {status} [delay: {res.delay}]"
-
-        logging.info(msg)
-    #endregion
-
-    #region Exec pings
-    async def execPings(sleep : bool = True):
-        # Async ping
-        if args.pingAsync:
-            results = await Pinger.pingAllAsync(
-                args.hosts,
-                args.timeout,
-                args.resolve)
-
-        # Synchronous ping
-        else:
-            results = Pinger.pingAll(
-                args.hosts,
-                args.timeout,
-                args.resolve)
-
-        for res in results:
-            logPingResult(res)
-
-        if (args.continuous or args.repeat > 1) and sleep: 
-            time.sleep(args.delay)
-    #endregion
+    if (args.continuous or args.repeat > 1) and sleep: 
+      time.sleep(args.delay)
+  #endregion
 
 
-    try:
-        while args.continuous:
-            await execPings()
+  try:
+    while args.continuous:
+      await execPings()
 
-        for i in range(args.repeat):
-            await execPings(i != (args.repeat - 1))
-    except:
-        return
+    for i in range(args.repeat):
+      await execPings(i != (args.repeat - 1))
+  except:
+    return
 #endregion
 
 
